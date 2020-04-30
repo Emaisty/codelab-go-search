c8ef62a415180aefbf135862f03d4784df0df642
Emaisty <maxgusevfff@yandex.ru>
2020-04-30 15:02:42 +0300

    add parallel call check file

--- main.go
+++ main.go
@@ -2,122 +2,171 @@ package main
 
 import (
 	"bufio"
-	"flag"
+	"crypto/md5"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 )
 
-var (
-	numberofstring = flag.Bool("n", false, "print file ")
-)
+type HashResult struct {
+	file string
+	hash string
+	err  error
+}
 
 type ScanResult struct {
 	file       string
 	lineNumber int
 	line       string
+	err        error
 }
 
-func scanFile(fpath, pattern string) ([]ScanResult, error) {
+func scanFile(fpath, pattern string) (r ScanResult) {
 	f, err := os.Open(fpath)
 	if err != nil {
-		return nil, err
+		r.err = err
+		return
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
-	var result []ScanResult
 	counter := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if strings.Contains(line, pattern) {
-			linewithpattern := ScanResult{file: fpath, lineNumber: counter, line: line}
-			result = append(result, linewithpattern)
+			r.file = fpath
+			r.lineNumber = counter
+			r.line = line
+			r.err = nil
 		}
 		counter++
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return
 	}
-	return result, nil
+	return
 }
 
-func exit(format string, val ...interface{}) {
-	if len(val) == 0 {
-		fmt.Println(format)
-	} else {
-		fmt.Printf(format, val)
-		fmt.Println()
-	}
-	os.Exit(1)
-}
-
-func processFile(fpath string, pattern string) {
-
-	res, err := scanFile(fpath, pattern)
+func md5File(path string) (r HashResult) {
+	r.file = path
+	f, err := os.Open(path)
 	if err != nil {
-		exit("Error scanning %s: %s", fpath, err.Error())
+		r.err = err
+		return
 	}
-	if *numberofstring {
-		for _, line := range res {
-			fmt.Println(line.file, ":", line.lineNumber, ":", line.line)
-		}
-	} else {
-		for _, line := range res {
-			fmt.Println(line.file, ":", line.line)
-		}
+	defer f.Close()
+
+	h := md5.New()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		r.err = err
+		return
 	}
+	hashBytes := h.Sum(nil)
+	r.err = nil
+	r.hash = fmt.Sprintf("%x", hashBytes)
+	return
 }
 
-func gorut(fpath string, pattern string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	processFile(fpath, pattern)
+func walkDirectory(done chan bool, root string) (chan string, chan error) {
+	files := make(chan string)
+	// Канал в котором можно оставить 1 элемент(1 ошибку)
+	errc := make(chan error, 1)
+	go func() {
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			select {
+			case <-done:
+				return errors.New("walk canceled by user")
+			default: // Nothing.
+			}
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				files <- path
+			}
+			return nil
+		})
+		errc <- err
+		close(files)
+	}()
+	return files, errc
 }
 
-func processDirectory(dir string, pattern string) {
+func md5Files(files chan string) chan HashResult {
+	res := make(chan HashResult)
+	go func() {
+		for f := range files {
+			res <- md5File(f)
+		}
+		close(res)
+	}()
+	return res
+}
+
+func md5FilesParallel(done chan bool, files chan string, n int, pattern string) chan ScanResult {
+	res := make(chan ScanResult)
+	if n == 0 {
+		fmt.Println("KEK")
+		go func() {
+			var wg sync.WaitGroup
+			for f := range files {
+				wg.Add(1)
+				go func() {
+					select {
+					case <-done: //Canceled
+					case res <- scanFile(f, pattern): // OK
+					}
+					wg.Done()
+				}()
+			}
+			wg.Wait()
+			close(res)
+		}()
+		return res
+	}
 	var wg sync.WaitGroup
-	var paths []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		paths = append(paths, path)
-		return nil
-	})
-	if err != nil {
-		panic(err)
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			for f := range files {
+				select {
+				case <-done: // Canceled
+				case res <- scanFile(f, pattern): // OK
+				}
+			}
+			wg.Done()
+		}()
 	}
-	for _, path := range paths {
-		info, err := os.Stat(path)
-		if err != nil {
-			panic(err)
-		}
-		if !info.IsDir() {
-			wg.Add(1)
-			go gorut(path, pattern, &wg)
-		}
-
-	}
-	wg.Wait()
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+	return res
 }
 
 func main() {
-	flag.Parse()
-
-	if flag.NArg() < 2 {
-		exit("usage: go-search <path> <pattern> to search")
+	done := make(chan bool)
+	start := time.Now()
+	files, errc := walkDirectory(done, ".")
+	pattern := "kek"
+	// Синтетика.
+	//go func() {
+	//    time.Sleep(1 * time.Second)
+	//    close(done)
+	//}()
+	//results := md5Files(files)
+	results := md5FilesParallel(done, files, 20, pattern)
+	for res := range results {
+		if res.file != "" {
+			fmt.Println(res.file, res.lineNumber, res.line, res.err)
+		}
 	}
-
-	path := flag.Arg(0)
-	pattern := flag.Arg(1)
-
-	info, err := os.Stat(path)
-	if err != nil {
-		panic(err)
-	}
-
-	if info.IsDir() {
-		processDirectory(path, pattern)
-	} else {
-		processFile(path, pattern)
-	}
+	fmt.Println(<-errc)
+	fmt.Println("Elapsed: ", time.Now().Sub(start))
 }
